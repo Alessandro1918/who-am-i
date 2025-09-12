e069c7b5caea82043a574a95c466d4c1c769fa7b
Alessandro1918 <alessandro1918@hotmail.com>
2025-09-12 17:56:30 -0300

    fix: add error treatment to IP API calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,43 +18,63 @@ export default function Home() {
   const [ deviceScreen, setDeviceScreen ] = useState("-")
   const [ deviceModel, setDeviceModel ] = useState("-")
   const [ deviceManufacturer, setDeviceManufacturer ] = useState("-")
-  const [ geolocationSource, setGeolocationSource ] = useState("ip")
-  const [ geolocationError, setGeolocationGpsError ] = useState("-")
+  const [ geolocationSource, setGeolocationSource ] = useState("-")
   const [ geolocationLatitude, setGeolocationLatitude ] = useState(0)
   const [ geolocationLongitude, setGeolocationLongitude ] = useState(0)
-  const [ geolocationAccuracy, setGeolocationAccuracy ] = useState(0)
+  const [ geolocationAccuracy, setGeolocationAccuracy ] = useState(0) //https://iplogger.org/ip-tracker/
+  const [ geolocationCity, setGeolocationGpsCity ] = useState("-")
+  const [ geolocationCountry, setGeolocationGpsCountry ] = useState("-")
+  const [ geolocationMessage, setGeolocationGpsMessage ] = useState("-")
+
 
   useEffect(() => {
 
     //Get IP:
-    fetch("https://api.ipify.org?format=json").then(response => {
-      response.json().then(json => {
+    (async () => {
+      //IP V4:
+      try {
+        // const response = await fetch("https://api.ipify.org?format=json")
+        const response = await fetch("https://ipinfo.io/json")
+        if (response.status !== 200) { throw new Error("IPV4 API error") }
+        const json = await response.json()
         setIpv4(json.ip)
-      })
-    })
-    fetch("https://freeipapi.com/api/json").then(response => {
-      response.json().then(json => {
-        if (json.ipVersion === 6) {
-          setIpv6(json.ipAddress)
 
-          //Set Position details, before user grant GPS access
-          if (geolocationSource !== "gps") {
-            setGeolocationLatitude(json.latitude)
-            setGeolocationLongitude(json.longitude)
-            setGeolocationAccuracy(50000)  //from https://iplogger.org/ip-tracker/
-          }
+        //Get Position details, before user grant GPS access:
+        if (geolocationSource !== "gps") {
+          setGeolocationSource("ip")
+          setGeolocationLatitude(Number(json.loc.split(",")[0]))
+          setGeolocationLongitude(Number(json.loc.split(",")[1]))
+          setGeolocationGpsCity(json.city)
+          setGeolocationGpsCountry(json.country)
         }
-      })
-    })
+      } catch (err) {
+        console.log(err)
+        setIpv4("X")
+      }
 
-    //Set Platform details
+      //IP V6:
+      try {
+        // const response = await fetch("https://freeipapi.com/api/json")
+        const response = await fetch("https://api.iplocation.net/?cmd=get-ip")
+        if (response.status !== 200) { throw new Error("IPV6 API error") }
+        const json = await response.json()
+        if (json.ip_version == 6) {
+          setIpv6(json.ip)
+        }
+      } catch (err) {
+        console.log(err)
+        setIpv6("X")
+      }
+    })()
+
+    //Get Platform details:
     setPlatformBrowserName(platform.name)
     setPlatformBrowserVersion(platform.version)
     setPlatformOSName(platform.os.family)
     setPlatformOSVersion(platform.os.version)
     // setPlatformDescription(platform.description)
 
-    //Set Network details
+    //Get Network details:
     //https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation
     //OBS: API does not support Firefox or Safari
     if ("connection" in navigator) {
@@ -66,7 +86,7 @@ export default function Home() {
       setNetworkQuality(quality)
     }
 
-    //Set Device details
+    //Get Device details:
     //OBS: device detection by UA string, and not all of them have this info
     setDeviceType(screen.width > 768 ? "desktop" : "cellphone")
     setDeviceScreen(`${screen.width} x ${screen.height}`)
@@ -75,11 +95,11 @@ export default function Home() {
       setDeviceManufacturer(platform.manufacturer)
     }
 
-    //Set Position details, after user grant GPS access
+    //Get Position details, after user grant GPS access:
     //https://www.w3schools.com/html/html5_geolocation.asp
     //OBS: Mobile browsers, once location permission is denied, won't ever ask user for permission again. Enable location usage manually by clicking on the locker icon beside the URL.
     //OBS: Mobile browsers won't distinguish between GPS "on" or "off", even after location permission is granted.
-    function showPosition(position: any) {
+    function showGeolocationPosition(position: any) {
       setGeolocationSource("gps")
       // console.log(position)
       setGeolocationLatitude(position.coords.latitude)
@@ -87,28 +107,28 @@ export default function Home() {
       setGeolocationAccuracy(position.coords.accuracy)
     }
 
-    function showError(error: any) {
+    function showGeolocationError(error: any) {
       switch(error.code) {
         case error.PERMISSION_DENIED:
-          setGeolocationGpsError("User denied the request for Geolocation.")
+          setGeolocationGpsMessage("User denied the request for Geolocation.")
           break;
         case error.POSITION_UNAVAILABLE:
-          setGeolocationGpsError("Location information is unavailable.")
+          setGeolocationGpsMessage("Location information is unavailable.")
           break;
         case error.TIMEOUT:
-          setGeolocationGpsError("The request to get user location timed out.")
+          setGeolocationGpsMessage("The request to get user location timed out.")
           break;
         case error.UNKNOWN_ERROR:
-          setGeolocationGpsError("An unknown error occurred.")
+          setGeolocationGpsMessage("An unknown error occurred.")
           break;
       }
     }
 
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError)
+      navigator.geolocation.getCurrentPosition(showGeolocationPosition, showGeolocationError)
     }
     else {
-      setGeolocationGpsError("Geolocation is not supported by this browser.")
+      setGeolocationGpsMessage("Geolocation is not supported by this browser.")
     }
   }, [])
 
@@ -148,10 +168,12 @@ export default function Home() {
           },
           geolocation: {
             source: geolocationSource,
-            error: geolocationError,
             latitude: geolocationLatitude,
             longitude: geolocationLongitude,
-            accuracy: geolocationAccuracy
+            accuracy: geolocationAccuracy,
+            city: geolocationCity,
+            country: geolocationCountry,
+            message: geolocationMessage,
           }
         }} 
       />
